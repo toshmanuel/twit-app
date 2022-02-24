3035cca6207df67886cebe10c9e6208c29eaafac
toshmanuel <oladimejiomotosho5@gmail.com>
2022-02-24 12:19:13 +0100

    eureka service discovery

--- account/src/main/java/com/twitapp/account/AccountApplication.java
+++ account/src/main/java/com/twitapp/account/AccountApplication.java
@@ -2,8 +2,10 @@ package com.twitapp.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
