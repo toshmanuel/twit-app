43a7ff56beaf4124eaf5a8d12a442c3540ce1096
toshmanuel <oladimejiomotosho5@gmail.com>
2022-02-25 00:15:41 +0100

    services configuration for service discovery

--- tweet/src/main/java/com/twitapp/tweet/TweetApplication.java
+++ tweet/src/main/java/com/twitapp/tweet/TweetApplication.java
@@ -2,8 +2,10 @@ package com.twitapp.tweet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TweetApplication {
     public static void main(String[] args) {
         SpringApplication.run(TweetApplication.class, args);
